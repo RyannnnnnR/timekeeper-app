1781d135825f8f2a9853d6437c16281c4c4da38d
RyannnnnnnR <ryan.reichenberg@gmail.com>
2018-04-28 19:03:36 +1000

    UPDATE: Using SQL ORM for data handling

--- app.js
+++ app.js
@@ -1,50 +1,26 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const app = express()
-const db = require('mysql')
-var index = require('./api/index')
-var authorization = require('./api/authorization')
+const express = require('express');
+const app = express();
+const index = require('./api/index');
+const authorization = require('./api/authorization');
+const Sequelize = require('sequelize');
 
-var con = db.createConnection({
-  host: "localhost",
-  user: "root",
-  password: ""
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE IF NOT EXISTS timekeeperdb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-  con.query("USE timekeeperdb", function (err, result) {
-    if (err) throw err;
-    console.log("Database Selected");
-  });
-  con.query("CREATE TABLE IF NOT EXISTS Roles(id int, role varchar(255), PRIMARY KEY(id));", function (err, result) {
-    if (err) throw err;
-    console.log("Roles table created");
-  })
-con.query("CREATE TABLE IF NOT EXISTS Teams(id int auto_increment, name varchar(255), department varchar(255), PRIMARY KEY(id))", function (err, result) {
-  if (err) throw err;
-  console.log("Teams table created");
-})
-con.query("CREATE TABLE IF NOT EXISTS Users (id int auto_increment, fname varchar(255), lname varchar(255), role_id int, PRIMARY KEY(id), FOREIGN KEY(role_id) REFERENCES Roles(id))", function (err, result) {
-  if (err) throw err;
-  console.log("Users table created");
-})
-con.query("CREATE TABLE IF NOT EXISTS User_Teams(id int auto_increment, user_id int, team_id int, PRIMARY KEY(id), FOREIGN KEY(user_id) REFERENCES Users(id), FOREIGN KEY(team_id) REFERENCES Teams(id))", function (err, result) {
-  if (err) throw err;
-  console.log("User Team Intermediary created");
-})
-con.query("CREATE TABLE IF NOT EXISTS Entries(id int auto_increment, description  varchar(255), user_id int, team_id int, time_added time, date_added date, PRIMARY KEY(id), FOREIGN KEY(user_id) REFERENCES Users(id), FOREIGN KEY(team_id) REFERENCES Teams(id))", function (err, result) {
-  if (err) throw err;
-  console.log("Entries table created");
-})
-});
-app.use('/', index)
+
+app.use('/', index);
 app.post('/test', authorization.authorizeWithPermission('admin'), function(req,res){
   res.json({token: "HI"})
-})
+});
 
-app.listen(3000, () => console.log('Activated and listening to port 3000'))
+app.listen(3000, () => console.log('Activated and listening to port 3000'));
